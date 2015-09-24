7fa01e44b22c7dc73e5b409dae2e6e9c7c0c4863
unknown <RICHBAK@CILABS-119B-14.scilsnet.rutgers.edu>
2015-09-24 19:51:48 -0400

    Adding files

---
+++ Assets/Apple.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Apple : MonoBehaviour {
+
+	public static float bottomY = -20f;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.y < bottomY) {
+			Destroy(this.gameObject);
+			ApplePicker apScript = Camera.main.GetComponent<ApplePicker> ();
+			apScript.AppleDestroyed ();
+		}
+
+
+	}
+}
---
+++ Assets/Basket.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Basket : MonoBehaviour {
+
+	public GUIText scoreGT;
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 mousepos2D = Input.mousePosition;
+		mousepos2D.z = -Camera.main.transform.position.z;
+		
+		Vector3 mousepos3D = Camera.main.ScreenToWorldPoint (mousepos2D);
+		Vector3 pos = this.transform.position;
+		pos.x = mousepos3D.x;
+		this.transform.position = pos;
+	}
+
+	void Start () {
+		GameObject scoreGO = GameObject.Find("ScoreCounter");
+		scoreGT = scoreGO.GetComponent<GUIText> ();
+		scoreGT.text = "0";	
+	}
+
+	void OnCollisionEnter(Collision coll){
+		GameObject collidedWith = coll.gameObject;
+		if (collidedWith.tag == "Apple") {
+			Destroy(collidedWith);
+		}
+
+		int score = int.Parse (scoreGT.text);
+		score += 100;
+		scoreGT.text = score.ToString ();
+
+		if (score > HighScore.score) {
+			HighScore.score = score;
+		}
+	}
+}
